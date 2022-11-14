8e0040b877a27a9d9cf9ff377de9fe07c91f01c7
Jan Andrle <andrle.jan@centrum.cz>
2022-11-14 12:37:35 +0100

    :bookmark: Nový snapshot & statistiky dle instancí

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Jen narychlo spíchnutý script pro stahování/porovnání přírustku uživatelů dle [fedilist](http://demo.fedilist.com/instance?q=&ip=&software=mastodon&registrations=&onion=).
+Jen narychlo spíchnutý script pro stahování/porovnání přírustku uživatelů dle [fedilist](http://demo.fedilist.com/instance?q=&ip=&software=mastodon&registrations=&onion=) (popis získání data & autor: [FediList.com](https://fedilist.com/)).
 
 Vl. také slouží k testování [jaandrle/nodejsscript at dev/0.9](https://github.com/jaandrle/nodejsscript/tree/dev/0.9):
 - instalace: `npm install https://github.com/jaandrle/nodejsscript#dev/0.9`
--- fedilist.js
+++ fedilist.js
@@ -5,14 +5,28 @@ $.api()
 .command("compare-last", "Porovná dva posledni snapshoty (defaultně jen „.cz”)")
 .option("--all", "Zahrnout i ne-CZ.")
 .action(function({ all= false }= {}){
-	const [ yesterday, today ]= s.$()
+	const [ previous, last ]= s.$()
 		.ls("./mastodon-list--*.csv")
 		.slice(-2)
 		.map(fileToData)
-		.map(data=> all ? data : data.filter(([ domain ])=> isCz(domain)))
-		.map(usersCount);
-	echo({ today, yesterday });
-	echo({ diff: today-yesterday });
+		.map(data=> all ? data : data.filter(([ domain ])=> isCz(domain)));
+
+	const css= echo.css(
+		".nadpis { color: yellow; }",
+		".list_item{ margin-left: 4; }",
+		".users { color: magenta; }",
+		".diff { color: gray; }",
+	);
+	echo("%cUživatelé za instanci:", css.nadpis);
+	for(const row of last){
+		const [ domain, users_now ]= getDomainUsers(row);
+		const [ , users_prev ]= getDomainUsers(previous.find(([ domain_prev ])=> domain_prev===domain));
+		echo(`%c${domain}: %c~${users_now}%c (%crozdíl ~${users_now-users_prev}%c)`, css.list_item, css.users, css.unset, css.diff, css.unset);
+	}
+	
+	const [ all_previous, all_last ]= [ previous, last ].map(usersCount);
+	echo("%cUživatelé celkem:", css.nadpis);
+	echo(`%c%c~${all_last}%c (%crozdíl ~${all_last-all_previous}%c)`, css.list_item, css.users, css.unset, css.diff, css.unset);
 	$.exit(0);
 })
 .command("snapshot <name>", "Stáhne aktuální `csv` soubor a uloží jej `./mastodon-list--name.csv`.")
@@ -29,3 +43,4 @@ $.api()
 function usersCount(data){ return data.reduce((acc, row)=> acc+Number(row[3]), 0); }
 function fileToData(file_name){ return s.cat(file_name).split("\n").map(line=> line.split(",")).filter(([ _, state ])=> state==="up"); }
 function isCz(candidate){ return /\.cz$/.test(candidate) || candidate==="czech.social" || candidate==="boskovice.social"; }
+function getDomainUsers(row){ if(!row) return [ null, 0 ]; const d= row[0]; const u= Number(row[3]); return [ d, u ]; }
